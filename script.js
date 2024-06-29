3645a544754c2edfd62ee1f8d489cc603faf9689
Sasha <167915085+sashashreyder@users.noreply.github.com>
2024-06-29 18:51:27 -0300

    10 examples (var)

---
+++ script.js
@@ -0,0 +1,113 @@
+
+var cassete= {
+   "bandName": "NewBand",
+   "quantity": 100,
+   "canIBuyItOnline": true,
+   "canIBuyItOffline": false,
+   "MusiciansName": ["John", "Peter", "Ivan", "Fernando"],
+   "doesBandHasLiveShows": true,
+   "countryOfOrigin": "Brazil"
+}
+
+var Vikings= {
+    "typeOfShow": "Hystoic",
+    "quantityOfSeasons": 8,
+    "canIWatchItOnline": true,
+    "canIBuyItOnDvd": false,
+    "3BestCharacters": ["Ragnar", "Floki", "Lagertha"],
+    "awardsWon": true
+}
+
+var JohnnyDepp= {
+    "dateOfBirth": new Date('1963-06-09'),
+    "placeOfBirth": "UnitedStates",
+    "occupation": ["actor", "musician"],
+    "greatestRoleInTheMovie": "CaptainJackSparrow",
+    "awardsWon": true,
+    "height": 178,
+    "favoriteColor": "black",
+    "favoriteWine": "ChateauCalon-Segur"
+}
+
+var cat= {
+   "name": "Ruby",
+   "type": "cutiePie",
+   "occupation": "beingAdorable",
+   "color": "orange",
+   "shape": "round",
+   "weightInKilos": 10,
+   "age": 3
+}
+
+var food= {
+    "name": ["batata", "potato"],
+    "surname": "frita",
+    "quantityInKilos": 100,
+    "color": "brown",
+    "placeOfUsing": ["kitchen", "diningRoom"],
+    "bestSauce": ["ketchup", "barbecue", "mustard"]
+}
+
+var cartoon= {
+    "name": "FamilyGuy",
+    "genreOfTheCartoon": "comedy", 
+    "canIWatchItOnline": true,
+    "canIBuyItOnDvd": false,
+    "3BestCharacters": ["Louis", "Peter", "Stewie"],
+    "awardsWon": false
+}
+
+var dress= {
+    "brand": "Chanel",
+    "name": "wave",
+    "type": "cocktailDress",
+    "color": "blue",
+    "pattern": false,
+    "size": "medium",
+    "length": "midi",
+    "sleeves": false,
+    "canIBuyItOnline": true,
+    "canIBuyItInTheShop": true,
+    "worldwideShipping": true,
+    "otherColorsInStock": true,
+    "otherSizesInStock": true,
+    "discounts": false,
+}
+
+var Hearthstone= {
+    "type": "onlineGame",
+    "genre": ["advanture", "cardGame"],
+    "developer": "blizzard",
+    "relatedGames": ["WorldOfWarcraft", "Warcraft"],
+    "differentGameModesAvailible": true,
+    "plauWithFriends": true,
+    "playOffline": false,
+    "browserGame": false,
+    "inGamePurchases": true,
+    "differentCharacters": true
+}
+
+var Country= {
+    "name": "Brazil",
+    "continent": "southAmerica",
+    "population": 215000000,
+    "largestEthnicGroup": "whiteBraziians",
+    "size": 8510000,
+    "popularFood": ["brigadeiro", "paoDeQueijo", "pastel", "farofa"],
+    "largestCity": "SaoPaulo",
+    "mostDangerousCity": "rioDeJaneiro",
+    "climate": "tropical",
+    "currency": "brazilianReal",
+    "mostPopularSport": "football",
+    "language": "brazilianPortuguese",
+    "famousFor": ["Samba", "football", "carnaval", "RioDeJaneiro", "foodAndDrinks"]
+}
+
+var Book= {
+    "name": "HarryPotter",
+    "howManyParts": 7,
+    "author": "JKRowling",
+    "yearOfPublishing": 1998,
+    "movie": true,
+    "mainMovieStars": ["EmmaWatson", "DanielRadcliffe", "RupertGrint"],
+}
